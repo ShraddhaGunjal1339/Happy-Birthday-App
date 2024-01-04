9e697659898a41a639841ca626f78c04fe05f6c0
Shraddha <shraddhagunjal1037@gmail.com>
2024-01-04 12:26:23 +0530

    Message changed

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun HappyBirthdayPreview() {
     HappyBirthdayTheme {
-        Greeting("Samrth ")
+        Greeting("Samrth Arvind gunjal")
     }
 }

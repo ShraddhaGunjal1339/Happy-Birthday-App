b68a82fdec82e4b8f186875c8f2338ede1c092a7
Shraddha <shraddhagunjal1037@gmail.com>
2024-01-05 09:45:47 +0530

    Add alignment

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -25,7 +30,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Birthday Samrth" , from = "Shraddha")
                    // Greeting("Android")
+
                 }
             }
         }
@@ -55,18 +62,27 @@ fun GreetingText(message: String,from : String , modifier: Modifier = Modifier) 
 //            lineHeight = 21.sp
 //        )
 //    }
-    Column (modifier = modifier){
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp
-        )
-        Text(
-            text = from,
-            fontSize = 20.sp,
-            lineHeight = 116.sp
-        )
-    }
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+            Text(
+                text = message,
+                fontSize = 100.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = from,
+                fontSize = 20.sp,
+                lineHeight = 116.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.End)
+
+                //textAlign = TextAlign.Center
+            )
+        }
 
 }
 
@@ -74,7 +90,7 @@ fun GreetingText(message: String,from : String , modifier: Modifier = Modifier) 
 @Composable
 fun HappyBirthdayPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday samrth !" ,"From your Sister Shraddha")
+        GreetingText("Happy Birthday Sakshi !" ,"From your Sister Shraddha")
         //Greeting("Samrth Arvind gunjal")
 
     }

c4c6048cef77a8e4f799cf1fab136514e050a523
Shraddha <shraddhagunjal1037@gmail.com>
2024-01-05 11:56:41 +0530

    Added image to background

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,11 +79,18 @@ fun GreetingText(message: String,from : String , modifier: Modifier = Modifier) 
 
 }
 
+@Composable
+fun GreetingImage(message: String , from: String , modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(painter = image , contentDescription = null)
+}
+
 @Preview(showBackground = true ,name = "Shraddha")
 @Composable
 fun HappyBirthdayPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday Samrth !" ,"From your Sister Shraddha")
+        GreetingImage("Happy Birthday samrth ","Shraddha")
+        //GreetingText("Happy Birthday Samrth !" ,"From your Sister Shraddha")
         //Greeting("Samrth Arvind gunjal")
 
     }
